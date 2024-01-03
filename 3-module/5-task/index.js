72ffebddb00ccbeb9333541422de06f0f917472f
D:\учработа <savenko_andrey95@mail.ru>
2024-01-03 18:03:36 +0300

    task done

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,21 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str
+    .split(" ")
+    .filter((str) => +str)
+    .map((str) => +str)
+    .sort((a, b) => {
+      if (a > b) {
+        return 1;
+      }
+      if (a < b) {
+        return -1;
+      }
+      return 0;
+    });
+
+  let last = arr.length - 1;
+  let max = arr[last];
+  let min = arr[0];
+
+  return { min, max };
 }
