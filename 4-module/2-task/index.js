2e0319ca9d3c8b2dbd1c2a306961041d820706ca
D:\учработа <savenko_andrey95@mail.ru>
2024-01-07 19:42:10 +0300

    remove console.log

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,6 +1,5 @@
 function makeDiagonalRed(table) {
   let len = table.rows.length;
-  console.log(len);
   for (let i = 0; i < len; i++) {
     table.rows[i].cells[i].style.backgroundColor = "red";
   }
