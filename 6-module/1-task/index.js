bdadf6112b19aa3c20ddc2789d1fd8b3781ca233
D:\учработа <savenko_andrey95@mail.ru>
2024-01-16 20:35:42 +0300

    task done

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,50 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = document.createElement("table");
+    this.createTable();
+  }
+
+  createTable() {
+    this.elem.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+        </tr>
+      </thead>
+    `
+    );
+
+    let tbody = document.createElement("tbody");
+    this.elem.append(tbody);
+
+    this.rows.forEach((element) => {
+      tbody.insertAdjacentHTML(
+        "afterbegin",
+        `
+        <tr>
+          <td>${element.name}</td>
+          <td>${element.age}</td>
+          <td>${element.salary}</td>
+          <td>${element.city}</td>
+          <td><button>X</button></td>
+        </tr>
+        `
+      );
+    });
+
+    let arrButton = this.elem.querySelectorAll("button");
+
+    for (let button of arrButton) {
+      button.addEventListener("click", (event) => {
+        event.target.closest("tr").remove();
+      });
+    }
   }
 }
